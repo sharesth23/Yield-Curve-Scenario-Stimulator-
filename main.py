f23d42c08cfc3ed5cf6642ff13a9e319f99edbf9
Sharesth <sharesth444yt@gmail.com>
2026-01-07 23:02:15 +0530

    altered code of my repo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from scenarios.steepener import steepener
+from scenarios.steepner import steepner
 from visualization.plot_curves import plot_curves
 
 df = pd.read_csv("data/gov_yields.csv")
